cc55a43c8b838c30a486880b218f43dd4b03386b
MJomaba <witek1987@gmail.com>
2017-02-10 12:37:43 +0000

    SMC volatility + start RMD

--- R/Volatility_SIS.R
+++ R/Volatility_SIS.R
@@ -35,19 +35,23 @@ Xp[,1]<-rnorm(Np,mean=0, sd=sqrt(sigma^2/(1-alpha^2)))
 gammap[,1]<-dnorm(Xp[,1],mean=0, sd=sqrt(sigma^2/(1-alpha^2)))*dnorm(Y[1],mean=0,sd=beta*exp(Xp[,1]/2))
 wp[,1]<-gammap[,1]/dnorm(Xp[,1],mean=0, sd=sqrt(sigma^2/(1-alpha^2)))
 Wp[,1]<-wp[,1]/sum(wp[,1])
-index_T<-rep(0,Np)
+index_T<-rep(0,N)
+index_2<-rep(0,Np)
 
 #Sequential calculation of particles and importance weights
 for(i in 2:N)
 {
   #IS step
   Xp[,i]<-rnorm(Np,mean=alpha*Xp[,i-1], sd=sigma)
-  gammap[,i]<-gammap[,i-1]*dnorm(Xp[,i],mean=alpha*Xp[,i-1], sd=1)*dnorm(Y[i],mean=0,sd=beta*exp(Xp[,i]/2))
-  wp[,i]<-gammap[,i]/dnorm(Xp[,i],mean=alpha*Xp[,i-1], sd=sigma)
+  #gammap[,i]<-gammap[,i-1]*dnorm(Xp[,i],mean=alpha*Xp[,i-1], sd=1)*dnorm(Y[i],mean=0,sd=beta*exp(Xp[,i]/2))
+  #wp[,i]<-gammap[,i]/dnorm(Xp[,i],mean=alpha*Xp[,i-1], sd=sigma)
+  wp[,i]<-dnorm(Xp[,i],mean=alpha*Xp[,i-1], sd=1)*dnorm(Y[i],mean=0,sd=beta*exp(Xp[,i]/2))/dnorm(Xp[,i],mean=alpha*Xp[,i-1], sd=sigma)
   Wp[,i]<-wp[,i]/sum(wp[,i])
   
   #Resampling step -using systematic resampling
   U1<-runif(1,min = 0,max = 1/Np)
+  if(i==2)
+    U2<-U1
   cumWj<-0
   lU<-U1
   index<-1
@@ -57,15 +61,15 @@ for(i in 2:N)
     cumWj<-cumWj+Wp[j,i]
     if(lU<cumWj) #test if at least one Uk is between two corresponding cumulative Wp
     {
-      Nji<-floor((cumWj-cumWjminus)*Np) 
+      Nji<-1+floor((cumWj-lU)*Np) 
       Xrp[index:(index+Nji-1),i]<-Xp[j,i]
       lU<-lU+Nji/Np
       index<-index+Nji
     }
   }
-  #Xp[,i]<-Xrp[,i]
-  #Wp[,i]<-1/Np
-  index_T[i]<-index
+  Xp[,i]<-Xrp[,i]
+  Wp[,i]<-1/Np
+  index_T[i]<-index-1
 }
   
 #Compute the mean particles path
@@ -79,7 +83,4 @@ for(i in 1:N)
   mu_Xp[i]<-sum(Xp[,i]*Wp[,i])
   points(i,mu_Xp[i],pch=19,col="#00aa0066")
 }
-
-  
-  
 
